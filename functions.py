9f365499703b7507df6384cf8859e9eb4fec087f
NoahBolger321 <ncbolger@uwaterloo.ca>
2019-03-22 14:20:27 -0400

    create loop, div by zero errors at this step, auth: CM, NB'

--- functions.py
+++ functions.py
@@ -3,38 +3,22 @@ import numpy as np
 from astropy import constants as ast_const
 
 # defining constants
-h_bar = ast_const.hbar
-G = ast_const.G
-c = ast_const.c
-m_e = ast_const.m_e
-m_p = ast_const.m_p
-k_b = ast_const.k_B
-sigma_SB = ast_const.sigma_sb
+h_bar = ast_const.hbar.value
+G = ast_const.G.value
+c = ast_const.c.value
+m_e = ast_const.m_e.value
+m_p = ast_const.m_p.value
+k_b = ast_const.k_B.value
+r_sun = ast_const.R_sun.value
+sigma_SB = ast_const.sigma_sb.value
 gamma = 5/3
-mew_mp = (1/((0.7)*2 + 0.75*(0.28) + 0.5*(0.02)))*m_p
 a = 4*sigma_SB / c
 
-
-# units rho_c: [kg/m**3]
-rho_c = 56.55*1000
-#units T_c: [K]
-T_c = 3056
-
-
-# epsilon values (PP, CNO)
-def eps_pp(rho, T):
-    return (1.07E-7)*(rho*1E-5)*(0.7**2)*((T*1E-6)**4)
-
-
-def eps_cno(rho, T):
-    return (8.24E-26)*(rho*1E-5)*(0.7)*(0.03*0.7)*((T*1E-6)**19.9)
-
-eps = eps_pp(rho, T) + eps_cno(rho, T)
-
-
-M_0 = (4*math.pi / 3)*(r_0**3)*rho_c
-lum = (4*math.pi / 3)*(r_0**3)*rho_c*eps
-
+# mass fractions
+X = 0.7
+Y = 0.28
+Z = 0.02
+mew_mp = (1/((X)*2 + 0.75*(Y) + 0.5*(Z)))*m_p
 
 # function returning Pressure
 def P(rho, T):
@@ -84,30 +68,96 @@ def dT_dR(kappa, rho, rad, T, lum, mass, press):
     return np.min([first, second])
 
 
-# runge kutta coefficients 1st Order
-l0 = h*dT_dR(kappa, rho, rad, T, lum, mass, press)
-k0 = h*drho_dR(rho, rad, mass)
-m0 = h*dM_dR(rho)
-n0 = h*dL_dR(rho, rad, eps)
-p0 = h*dTau_dR(rho, kappa)
+# units rho_c: [kg/m**3]
+rho_c = 56.55*1000
+#units T_c: [K]
+T_c = 8.23E6
 
-# runge kutta coefficients 2nd Order
-l1 = h*dT_dR(kappa, rho+0.5*k0, rad+0.5*h, T+0.5*l0, lum+0.5*n0, mass+0.5*m0, press)
-k1 = h*drho_dR(rho+0.5*k0, rad+0.5*h, mass+0.5*m0)
-m1 = h*dM_dR(rho+0.5*k0)
-n1 = h*dL_dR(rho+0.5*k0, rad+0.5*h, esp)
-p1 = h*dTau_dr(rho+0.5*k0, kappa)
 
-# runge kutta coefficients 3rd Order
-l2 = h*dT_dR(kappa, rho+0.5*k1, rad+0.5*h, T+0.5*l1, lum+0.5*n1, mass+0.5*m1, press)
-k2 = h*drho_dR(rho+0.5*k1, rad+0.5*h, mass+0.5*m1)
-m2 = h*dM_dR(rho+0.5*k1)
-n2 = h*dL_dR(rho+0.5*k1, rad+0.5*h, esp)
-p2 = h*dTau_dr(rho+0.5*k1, kappa)
+# epsilon values (PP, CNO)
+def eps_pp(rho, T):
+    return (1.07E-7)*(rho*1E-5)*(X**2)*((T*1E-6)**4)
 
-# runge kutta coefficients 4th Order
-l3 = h*dT_dR(kappa, rho+0.5*k2, rad*h, T+0.5*l2, lum+0.5*n2, mass+0.5*m2, press)
-k3 = h*drho_dR(rho+0.5*k2, rad*h, mass+0.5*m2)
-m3 = h*dM_dR(rho+0.5*k2)
-n3 = h*dL_dR(rho+0.5*k2, rad*h, esp)
-p3 = h*dTau_dr(rho+0.5*k2, kappa)
+
+def eps_cno(rho, T):
+    return (8.24E-26)*(rho*1E-5)*(X)*(0.03*X)*((T*1E-6)**19.9)
+
+T = T_c
+rho = rho_c
+
+r_0 = 0.01
+eps_0 = eps_pp(rho_c, T_c) + eps_cno(rho_c, T_c)
+M_0 = (4*math.pi / 3)*(r_0**3)*rho_c
+lum_0 = (4*math.pi / 3)*(r_0**3)*rho_c*eps_0
+
+M = M_0
+lum = lum_0
+
+# kappa values
+def kappa_func(T):
+    kap_es = (0.02*(1 + X))
+    kap_ff = (1E24)*(Z + 0.0001)*((rho*1E-3)**0.7)*(T**(-3.5))
+    kap_H = (2.5E-32)*(Z/0.02)*((rho*1E-3)**0.5)*(T**9)
+    return 1 / (1/kap_H + 1/(np.max([kap_es, kap_ff])))
+
+# RK step size
+h = 100000
+r_surf = 0.865*r_sun
+for rad in np.arange(0.01, r_surf, h):
+    # these will update with the 'new' values each time we loop through
+    # kappa function
+    kappa = kappa_func(T)
+    # pressure calculation
+    press = P(rho, T)
+    # espilon calculation
+    eps = eps_pp(rho, T) + eps_cno(rho, T)
+
+    # temperature DE
+    dTR = dT_dR(kappa, rho, rad, T, lum, M, press)
+    dPT = dP_dT(rho, T)
+    dPrho = dP_drho(rho, T)
+
+    # runge kutta coefficients 1st Order
+    l0 = h * dT_dR(kappa, rho, rad, T, lum, M, press)
+    k0 = h * drho_dR(rho, rad, M)
+    m0 = h * dM_dR(rho, rad)
+    n0 = h * dL_dR(rho, rad, eps)
+    # p0 = h * dTau_dR(rho, kappa)
+
+    # runge kutta coefficients 2nd Order
+    l1 = h * dT_dR(kappa, rho + 0.5 * k0, rad + 0.5 * h, T + 0.5 * l0, lum + 0.5 * n0, M + 0.5 * m0, press)
+    k1 = h * drho_dR(rho + 0.5 * k0, rad + 0.5 * h, M + 0.5 * m0)
+    m1 = h * dM_dR(rho + 0.5 * k0, rad)
+    n1 = h * dL_dR(rho + 0.5 * k0, rad + 0.5 * h, eps)
+    # p1 = h * dTau_dR(rho + 0.5 * k0, kappa)
+
+    # runge kutta coefficients 3rd Order
+    l2 = h * dT_dR(kappa, rho + 0.5 * k1, rad + 0.5 * h, T + 0.5 * l1, lum + 0.5 * n1, M + 0.5 * m1, press)
+    k2 = h * drho_dR(rho + 0.5 * k1, rad + 0.5 * h, M + 0.5 * m1)
+    m2 = h * dM_dR(rho + 0.5 * k1, rad)
+    n2 = h * dL_dR(rho + 0.5 * k1, rad + 0.5 * h, eps)
+    # p2 = h * dTau_dR(rho + 0.5 * k1, kappa)
+
+    # runge kutta coefficients 4th Order
+    l3 = h * dT_dR(kappa, rho + 0.5 * k2, rad * h, T + 0.5 * l2, lum + 0.5 * n2, M + 0.5 * m2, press)
+    k3 = h * drho_dR(rho + 0.5 * k2, rad * h, M + 0.5 * m2)
+    m3 = h * dM_dR(rho + 0.5 * k2, rad)
+    n3 = h * dL_dR(rho + 0.5 * k2, rad * h, eps)
+    # p3 = h * dTau_dR(rho + 0.5 * k2, kappa)
+
+    # new temperature
+    T = T + (1/6.0)*(l0+2*l1+2*l2+l3)
+    # new denisty
+    rho = rho + (1/6.0)*(k0+2*k1+2*k2+k3)
+    # new mass
+    M = M + (1/6.0)*(m0+2*m1+2*m2+m3)
+    # new luminosity
+    lum = lum  + (1/6.0)*(n0+2*n1+2*n2+n3)
+    # new tau
+    # tau = tau + (1/6.0)*(p0+2*p1+2*p2+p3)
+
+print("Temperature: {}".format(T))
+print("Density: {}".format(rho))
+print("Mass: {}".format(M))
+print("Luminosity: {}".format(lum))
+# print("Opactiy: {}".format(tau))
