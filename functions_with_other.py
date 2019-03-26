c7e1fd0448626537e5ee3da16f83c40f1e1f80c2
Yilda <yboukhto@uwaterloo.ca>
2019-03-26 15:33:13 -0400

    Edit functions.py to work in venv. Added functions_with_other to add plotting luminosity for C and He cores

--- functions_with_other.py
+++ functions_with_other.py
@@ -1,7 +1,8 @@
 import math
 import numpy as np
 from astropy import constants as ast_const
-import matplotlib
+# had to add lines 5-6, to get matplotlib to work in my virtual environment
+import matplotlib 
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
@@ -22,7 +23,7 @@ X = 0.7
 Y = 0.28
 Z = 0.02
 mew_mp = (1 / ((X) * 2 + 0.75 * (Y) + 0.5 * (Z))) * m_p
-
+X_C = 0.7 # 0.7 is a filler value to run the code with
 
 # function returning Pressure
 def P(rho, T):
@@ -87,6 +88,13 @@ def eps_pp(rho, T):
 def eps_cno(rho, T):
     return (8.24E-26) * (rho * 1E-5) * (X) * (0.03 * X) * ((T * 1E-6) ** 19.9)
 
+#epsilon values of other main sequences (Helium, Carbon)
+def eps_he(rho, T):
+    return (3.85E-8) * (Y ** 3) * ((rho * 1E-5) ** 2) * ((T * 1E-8)**44)
+
+def eps_cc(rho, T):
+    return (5E4) * (X_C ** 2) * (rho * 1E-5) * ((T * 1E-9) **30)
+
 
 # starting temperature and density at "centre" values
 T = T_c
@@ -95,15 +103,20 @@ rho = rho_c
 # intial values for radius, epsilon, mass and luminosity
 r_0 = 0.01
 eps_0 = eps_pp(rho_c, T_c) + eps_cno(rho_c, T_c)
+eps_0_he = eps_he(rho_c, T_c)
+eps_0_cc = eps_cc(rho_c, T_c)
 M = (4 * math.pi / 3.0) * (r_0 ** 3) * rho_c
 lum = (4 * math.pi / 3.0) * (r_0 ** 3) * rho_c * eps_0
+lum_he = (4 * math.pi / 3.0) * (r_0 ** 3) * rho_c * eps_0_he
+lum_cc = (4 * math.pi / 3.0) * (r_0 ** 3) * rho_c * eps_0_cc
 
 # empty storage arrays for plotting purposes
 M_vals = []
 rho_vals = []
 T_vals = []
 L_vals = []
-
+L_he_vals = []
+L_cc_vals = []
 
 # kappa values
 def kappa_func(T):
@@ -118,7 +131,7 @@ h = 10000
 r_surf = 0.75 * r_sun
 
 for rad in np.linspace(0.01, r_surf, 100000):
-    """This loops over a range of radius values ata  specified step size
+    """This loops over a range of radius values at a  specified step size
         - kappa, pressure and epsilon values will be updated after each iteration of the loop
         - this loop implements fourth order Runge Kutta numerical integration for the 5 DEs that need to be solved
         - the new values of temp, density, luminosity, mass and opacity are added to after each iteration as we are
@@ -131,6 +144,8 @@ for rad in np.linspace(0.01, r_surf, 100000):
     press = P(rho, T)
     # espilon calculation
     eps = eps_pp(rho, T) + eps_cno(rho, T)
+    epshe = eps_he(rho, T)
+    epscc = eps_cc(rho, T)
 
     # temperature DE
     dTR = dT_dR(kappa, rho, rad, T, lum, M, press)
@@ -142,6 +157,8 @@ for rad in np.linspace(0.01, r_surf, 100000):
     k0 = h * drho_dR(rho, rad, M)
     m0 = h * dM_dR(rho, rad)
     n0 = h * dL_dR(rho, rad, eps)
+    n0_he = h * dL_dR(rho, rad, epshe)
+    n0_cc = h * dL_dR(rho, rad, epscc)
     # p0 = h * dTau_dR(rho, kappa)
 
     # runge kutta coefficients 2nd Order
@@ -149,6 +166,8 @@ for rad in np.linspace(0.01, r_surf, 100000):
     k1 = h * drho_dR(rho + 0.5 * k0, rad + 0.5 * h, M + 0.5 * m0)
     m1 = h * dM_dR(rho + 0.5 * k0, rad + 0.5 * h)
     n1 = h * dL_dR(rho + 0.5 * k0, rad + 0.5 * h, eps)
+    n1_he = h * dL_dR(rho + 0.5 * k0, rad + 0.5 * h, epshe)
+    n1_cc = h * dL_dR(rho + 0.5 * k0, rad + 0.5 * h, epscc)
     # p1 = h * dTau_dR(rho + 0.5 * k0, kappa)
 
     # runge kutta coefficients 3rd Order
@@ -156,6 +175,8 @@ for rad in np.linspace(0.01, r_surf, 100000):
     k2 = h * drho_dR(rho + 0.5 * k1, rad + 0.5 * h, M + 0.5 * m1)
     m2 = h * dM_dR(rho + 0.5 * k1, rad + 0.5 * h)
     n2 = h * dL_dR(rho + 0.5 * k1, rad + 0.5 * h, eps)
+    n2_he = h * dL_dR(rho + 0.5 * k1, rad + 0.5 * h, epshe)
+    n2_cc = h * dL_dR(rho + 0.5 * k1, rad + 0.5 * h, epscc)
     # p2 = h * dTau_dR(rho + 0.5 * k1, kappa)
 
     # runge kutta coefficients 4th Order
@@ -163,6 +184,8 @@ for rad in np.linspace(0.01, r_surf, 100000):
     k3 = h * drho_dR(rho + k2, rad + h, M + m2)
     m3 = h * dM_dR(rho + k2, rad + h)
     n3 = h * dL_dR(rho + k2, rad + h, eps)
+    n3_he = h * dL_dR(rho + k2, rad + h, epshe)
+    n3_cc = h * dL_dR(rho + k2, rad + h, epscc)
     # p3 = h * dTau_dR(rho + 0.5 * k2, kappa)
 
     # new temperature
@@ -173,6 +196,10 @@ for rad in np.linspace(0.01, r_surf, 100000):
     M += (1 / 6.0) * (m0 + 2 * m1 + 2 * m2 + m3)
     # new luminosity
     lum += (1 / 6.0) * (n0 + 2 * n1 + 2 * n2 + n3)
+    # new helium luminosity
+    lum_he += (1 / 6.0) * (n0_he + 2 * n1_he + 2 * n2_he + n3_he)
+    # new carbon luminosity
+    lum_cc += (1 / 6.0) * (n0_cc + 2 * n1_cc + 2 * n2_cc + n3_cc)
     # new tau
     # tau += (1/6.0)*(p0+2*p1+2*p2+p3)
 
@@ -180,12 +207,17 @@ for rad in np.linspace(0.01, r_surf, 100000):
     rho_vals.append(rho)
     T_vals.append(T)
     L_vals.append(lum)
+    L_he_vals.append(lum_he)
+    L_cc_vals.append(lum_cc)
+    
 
 # print out final values fo each parameter for reference
 print("Temperature: {}".format(T))
 print("Density: {}".format(rho))
 print("Mass: {}".format(M))
 print("Luminosity: {}".format(lum))
+print("Helium Luminosity: {}".format(lum_he))
+print("Carbon Luminosity: {}".format(lum_cc))
 
 # plotting mass, density, temperature and luminosity as functions of r
 fig = plt.figure()
@@ -215,4 +247,18 @@ plt.title("Luminosity vs. Radius")
 plt.xlabel("Radius")
 plt.ylabel("Luminosity")
 # fig4.show()
+
+fig5 = plt.figure()
+plt.plot(np.linspace(0.01, r_surf, 100000), L_he_vals)
+plt.title("Helium Luminosity vs. Radius")
+plt.xlabel("Radius")
+plt.ylabel("Luminosity")
+# fig5.show()
+
+fig6 = plt.figure()
+plt.plot(np.linspace(0.01, r_surf, 100000), L_cc_vals)
+plt.title("Carbon Luminosity vs. Radius")
+plt.xlabel("Radius")
+plt.ylabel("Luminosity")
+# fig6.show()
 plt.show()
